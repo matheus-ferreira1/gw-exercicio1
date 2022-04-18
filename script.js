f38236e59b5cf097166c8fa732b98fece0bffbe6
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-18 20:47:51 -0300

    upload do projeto pronto

---
+++ script.js
@@ -0,0 +1,6 @@
+let meuArray = [69, 88, 8, 61, 12, 45, 98, 19, 54, 49, 17, 76];
+
+let maiorNumero = meuArray.sort((a, b) => a - b)[meuArray.length - 1]
+
+console.log(meuArray)
+console.log(`O maior número do array é: ${maiorNumero}`)
